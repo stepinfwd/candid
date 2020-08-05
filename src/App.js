6a2c84545813de135deae4b603e7535e4f89574f
stepinfwd <stepinfwd@gmail.com>
2020-08-06 03:18:04 +0530

    1 st deploy

--- src/App.js
+++ src/App.js
@@ -5,79 +5,91 @@ import InputComp from "./input.js";
 import Navbar from "./navbar";
 
 function App() {
-  const [search, setSearch] = useState([]);
+  const [search, setSearch] = useState("travel");
+  const [value, setValue] = useState([]);
+
   const [loaded, setIsLoaded] = React.useState(false);
+  const [imageData, setImageData] = useState([]);
+  const [sent, setSent] = useState(false);
 
-  useEffect((e) => {
-    // e.preventDefault();
-    const access_key = "LD3Wl2QrQPHFCUnAGXU6AWQdLSipmIgi8TZKjzOOxAI";
-    const url = "https://api.unsplash.com/photos/?client_id=YOUR_ACCESS_KEY";
-    axios
-      .get(
-        `https://api.unsplash.com/search/photos/?client_id=LD3Wl2QrQPHFCUnAGXU6AWQdLSipmIgi8TZKjzOOxAI&query=video`
-      )
+  useEffect(
+    (item) => {
+      // e.preventDefault();
+      const access_key = "LD3Wl2QrQPHFCUnAGXU6AWQdLSipmIgi8TZKjzOOxAI";
+      const url = "https://api.unsplash.com/photos/?client_id=YOUR_ACCESS_KEY";
+      axios
+        .get(
+          `https://api.unsplash.com/search/photos/?client_id=LD3Wl2QrQPHFCUnAGXU6AWQdLSipmIgi8TZKjzOOxAI&query=${search}`
+        )
 
-      .then((res) => {
-        console.log(res.data);
-        console.log("call");
-        setSearch(res.data.results);
-      });
-  }, []);
+        .then((res) => {
+          console.log(res.data);
+          console.log("call");
+          setValue(res.data.results);
+        });
+    },
+    [sent]
+  );
   console.log("stae", search);
-  const Modal = () => {
-    return (
-      <div>
-        <div className="modal" tabindex="-1" role="dialog">
-          <div className="modal-dialog" role="document">
-            <div className="modal-content">
-              <div className="modal-header">
-                <h5 className="modal-title">Modal title</h5>
-                <button
-                  type="button"
-                  className="close"
-                  data-dismiss="modal"
-                  aria-label="Close"
-                >
-                  <span aria-hidden="true">&times;</span>
-                </button>
-              </div>
-              <div className="modal-body">
-                <p>Modal body text goes here</p>
-              </div>
-              <div className="modal-footer">
-                <button type="button" className="btn btn-primary">
-                  Save changes
-                </button>
-                <button
-                  type="button"
-                  className="btn btn-secondary"
-                  data-dismiss="modal"
-                >
-                  Close
-                </button>
-              </div>
-            </div>
-          </div>
-        </div>
-      </div>
-    );
-  };
+  // const Modal = () => {
+  //   return (
+  //     <div>
+  //       <div className="modal" tabindex="-1" role="dialog">
+  //         <div className="modal-dialog" role="document">
+  //           <div className="modal-content">
+  //             <div className="modal-header">
+  //               <h5 className="modal-title">Modal title</h5>
+  //               <button
+  //                 type="button"
+  //                 className="close"
+  //                 data-dismiss="modal"
+  //                 aria-label="Close"
+  //               >
+  //                 <span aria-hidden="true">&times;</span>
+  //               </button>
+  //             </div>
+  //             <div className="modal-body">
+  //               <p>Modal body text goes here</p>
+  //             </div>
+  //             <div className="modal-footer">
+  //               <button type="button" className="btn btn-primary">
+  //                 Save changes
+  //               </button>
+  //               <button
+  //                 type="button"
+  //                 className="btn btn-secondary"
+  //                 data-dismiss="modal"
+  //               >
+  //                 Close
+  //               </button>
+  //             </div>
+  //           </div>
+  //         </div>
+  //       </div>
+  //     </div>
+  //   );
+  // };
 
   return (
     <div className="App">
-      <Navbar />
+      <Navbar
+        setSearch={setSearch}
+        search={search}
+        sent={sent}
+        setSent={setSent}
+      />
       {/* <InputComp /> */}
+      <h3 className="app-header">{search} photos</h3>
       <div className="image-grid" style={{ marginTop: "30px" }}>
-        {search &&
-          search.map((searchItem) => (
+        {value &&
+          value.map((searchItem) => (
             <>
               <img
                 className="search-image"
                 src={searchItem.urls.regular}
                 alt=""
                 key={searchItem.id}
-                name="kiran"
-                onClick={Modal}
+                // onClick={Modal}
               ></img>
             </>
           ))}
--- src/navbar.js
+++ src/navbar.js
@@ -2,11 +2,21 @@ import React from "react";
 
 function Navbar(props) {
   console.log("prop", props);
+
+  const changeHandler = (e) => {
+    props.setSearch(e.target.value);
+    console.log("search value", props.imageData);
+  };
+  const clickHandler = (e) => {
+    e.preventDefault();
+    console.log("k clicked");
+    props.setSent(!props.sent);
+  };
   return (
     <div>
       <nav
         className="navbar  justify-content-between"
-        style={{ backgroundColor: "  #2e151b" }}
+        style={{ backgroundColor: "  #17151B" }}
       >
         <p
           className="navbar-brand"
@@ -15,7 +25,6 @@ function Navbar(props) {
             fontWeight: "700",
             paddingTop: "1rem",
             fontSize: "2rem",
-            marginLeft: "2rem",
             letterSpacing: ".1rem",
           }}
         >
@@ -29,10 +38,12 @@ function Navbar(props) {
             type="search"
             placeholder="Search"
             aria-label="Search"
+            onChange={changeHandler}
           />
           <button
             className="btn btn-outline-success my-2 my-sm-0"
             style={{ color: "#fff", borderColor: "#fff", fontWeight: "800" }}
+            onClick={clickHandler}
           >
             Search
           </button>
